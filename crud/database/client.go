e045ca52dab0abc8a11fd25cb1f1603f9c6a0fe0
Anibrata Malkhandi <99401003+anibrata53@users.noreply.github.com>
2022-04-26 11:33:07 +0530

    Add files via upload

---
+++ crud/database/client.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"crud/entity"
+	"log"
+
+	"github.com/jinzhu/gorm"
+)
+
+//Connector variable used for CRUD operation's
+var Connector *gorm.DB
+
+//Connect creates MySQL connection
+func Connect(connectionString string) error {
+	var err error
+	Connector, err = gorm.Open("mysql", connectionString)
+	if err != nil {
+		return err
+	}
+	log.Println("Connection was successful!!")
+	return nil
+}
+func Migrate(table *entity.Person) {
+	Connector.AutoMigrate(&table)
+	log.Println("Table migrated")
+}
